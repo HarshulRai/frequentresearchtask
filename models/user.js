5c5c112c11815ef8d37db1c9639ff2d8308b10bb
Harshul Rai <61915312+HarshulRai@users.noreply.github.com>
2023-03-17 16:23:04 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
